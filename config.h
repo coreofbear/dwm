624fefea9ccef6f242d3c84af225a3759971d9b4
Mikhail Zaytsev <mikhailzayts@gmail.com>
2022-12-03 16:40:11 +0300

    added scratchpad patch

--- config.h
+++ config.h
@@ -89,6 +89,8 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[]  = { "chromium", NULL };
 static const char *telegramcmd[]  = { "telegram-desktop", NULL };
+static const char scratchpadname[]  = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "100x30", NULL };
 static const char *cmdprintscreen[]  = { "scrot", "-s", NULL };
 
 static Key keys[] = {
@@ -98,6 +100,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_b, 	   spawn,          {.v = browsercmd } },
 	{ MODKEY|ShiftMask,             XK_t, 	   spawn,          {.v = telegramcmd } },
+	{ MODKEY,                       XK_s,      togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
